0419aaae079ac2db0cb024fed5bce5a46c0e366c
vbahno <vbahno@softserveinc.com>
2022-12-16 22:30:50 +0200

    Refactored task2

--- src/main/java/homeWork5/Task2.java
+++ src/main/java/homeWork5/Task2.java
@@ -8,97 +8,106 @@ public class Task2 {
 
     private static final Logger logger = Logger.getLogger(Task2.class);
 
-//    б) Роздрукувати останній символ рядка.
     public static void printLastSymbolInText(String enteredText) {
 
         logger.info("Last symbol in entered text is: " + enteredText.charAt(enteredText.length() - 1));
     }
 
-    //    в) Перевірити, чи закінчується ваша рядок підрядком "!!!".
-    public static void verifyEndOfText(String enteredText, String endOfRow) {
+    public static void verifyIsEndsOfSubstringText(String enteredText) {
 
-        if(enteredText.contains(endOfRow)) {
-            logger.info("End of row is true: " + enteredText.endsWith(endOfRow));
+        Scanner scanner = new Scanner(System.in);
+        logger.info("Enter end of string: ");
+        String endOfString = scanner.next();
+
+        if(enteredText.contains(endOfString)) {
+            logger.info("Verify is end of substring in your entered text: " + enteredText.endsWith(endOfString));
         }
         else {
-            logger.error("Your entered text doesn't contain endOfRow");
+            logger.error("Your entered text '" + enteredText + "' doesn't contain that end of substring - " + endOfString);
         }
     }
 
-    //    г) Перевірити, чи починається ваша рядок підрядком "I like".
-    public static void verifyStartOfText(String enteredText, String startOfRow) {
+    public static void verifyIsStartOfSubstringText(String enteredText) {
 
-        if(enteredText.contains(startOfRow)) {
-            logger.info("Start of row is true: " + enteredText.startsWith(startOfRow));
+        Scanner scanner = new Scanner(System.in);
+        logger.info("Enter start of string: ");
+        String startOfString = scanner.next();
+
+        if(enteredText.contains(startOfString)) {
+            logger.info("Verify is start of substring in your entered text: " + enteredText.startsWith(startOfString));
         }
         else {
-            logger.error("Your entered text doesn't contain startOfRow");
-        }
-//        System.out.println("Start of row is true: " + enteredText.startsWith(startOfRow));
-    }
-
-    //    д) Перевірити, чи містить ваша рядок підрядок "Java".
-    public static void verifyContainsSubStringInText(String enteredText, String subString) {
-
-        if(enteredText.contains(subString)) {
-            logger.info("Your entered text contains substring '" + subString + "': " + enteredText.contains(subString));
-        }
-        else {
-            logger.error("Your entered text doesn't contain substring");
+            logger.error("Your entered text '" + enteredText + "' doesn't contain that start of substring - " + startOfString);
         }
     }
 
-    //e) Знайти позицію підрядка "Java" в рядку "I like Java !!!".
-    public static void findPositionOfSubStringInText(String enteredText, String subString) {
+    public static void verifyIsContainsSubstringInText(String enteredText) {
 
-        if(enteredText.contains(subString)) {
-            logger.info("Position of substring in your entered text: " + enteredText.indexOf(subString));
+        Scanner scanner = new Scanner(System.in);
+        logger.info("Enter substring for verifying in the entered text: ");
+        String substring = scanner.next();
+
+        if(enteredText.contains(substring)) {
+            logger.info("Your entered text contains substring '" + substring + "': " + enteredText.contains(substring));
         }
         else {
-            logger.error("Your entered text doesn't contain substring");
+            logger.error("Your entered text '" + enteredText + "' doesn't contain substring - " + substring);
         }
     }
 
-    // ж) Вивести на екран введену стрічку замінивши всі символи "а" на "о".
-    public static void replaceSymbolsInText(String enteredText, String from, String to) {
+    public static void findPositionOfSubstringInText(String enteredText) {
 
-        if(enteredText.contains(from)) {
-            logger.info("In your entered text were replaced symbols: " + enteredText.replace(from, to));
+        Scanner scanner = new Scanner(System.in);
+        logger.info("Enter substring for finding position in the entered text: ");
+        String substring = scanner.next();
+
+        if(enteredText.contains(substring)) {
+            logger.info("Position of substring '" + substring + "' in your entered text: " + enteredText.indexOf(substring));
         }
         else {
-            logger.error("Your entered text doesn't contain symbol for replacing");
+            logger.error("Your entered text '" + enteredText + "' doesn't contain substring - " + substring);
         }
     }
 
-    //    з) Перетворіть рядок до верхнього регістру.
+    public static void replaceSymbolsInText(String enteredText) {
+
+        Scanner scanner = new Scanner(System.in);
+        logger.info("Enter symbol, which you want to replace in the entered text: ");
+        String fromSymbol = scanner.next();
+        logger.info("Enter symbol, to which you want to replace in the entered text: ");
+        String toSymbol = scanner.next();
+
+        if(enteredText.contains(fromSymbol)) {
+            logger.info("In your entered text '" + enteredText + "' were replaced symbols: " + enteredText.replace(fromSymbol, toSymbol));
+        }
+        else {
+            logger.error("Your entered text '" + enteredText + "' doesn't contain symbol '" + fromSymbol + "' for replacing");
+        }
+    }
+
     public static void upperCaseForText(String enteredText) {
         logger.info("Upper case for your entered text: " + enteredText.toUpperCase());
     }
 
-    //    і) Перетворіть рядок до нижнього регістру.
     public static void lowerCaseForText(String enteredText) {
         logger.info("Lower case for your entered text: " + enteredText.toLowerCase());
     }
 
-    //    к) Вирізати рядок Java c допомогою методу String.substring() .
-    public static void cutSubStringFromText(String enteredText, String startSubstring, String endSubstring) {
+    public static void cutSubstringFromText(String enteredText) {
 
-        int startInt = enteredText.indexOf(startSubstring);
-        int endInt = enteredText.indexOf(endSubstring);
-        logger.info("Cut substring from your entered text: " + enteredText.substring(startInt, endInt));
+        Scanner scanner = new Scanner(System.in);
+        logger.info("Enter beginIndex of the entered text: ");
+        int startInt = scanner.nextInt();
+        logger.info("Enter endIndex of the entered text: ");
+        int endInt = scanner.nextInt();
+
+        if(enteredText.length() >= endInt && endInt > startInt) {
+            logger.info("From your entered text '" + enteredText + "' was cut substring: " + enteredText.substring(startInt, endInt));
+        }
+        else {
+            logger.error("From your entered text '" + enteredText + "' doesn't cut substring from " + startInt + " to " + endInt + " position");
+        }
     }
-
-
-//    а) Користувач вводить стрічку використовуючи Scanner. Наприклад: "I like Java !!!".
-//    б) Роздрукувати останній символ рядка.                                                    DONE
-//    в) Перевірити, чи закінчується ваша рядок підрядком "!!!".                                DONE
-//    г) Перевірити, чи починається ваша рядок підрядком "I like".                              DONE
-//    д) Перевірити, чи містить ваша рядок підрядок "Java".                                     DONE
-//    e) Знайти позицію підрядка "Java" в рядку "I like Java !!!".                              DONE
-//    ж) Вивести на екран введену стрічку замінивши всі символи "а" на "о".                     DONE
-//    з) Перетворіть рядок до верхнього регістру.                                               DONE
-//    і) Перетворіть рядок до нижнього регістру.                                                DONE
-//    к) Вирізати рядок Java c допомогою методу String.substring() .                            DONE
 
     public static void main(String[] args) {
 
@@ -107,14 +116,13 @@ public class Task2 {
         String enteredText = scanner.nextLine();
 
         printLastSymbolInText(enteredText);
-        verifyEndOfText(enteredText, "!!!");
-        verifyEndOfText(enteredText, "676");     // for false
-        verifyStartOfText(enteredText, "I like");
-        verifyContainsSubStringInText(enteredText, "Java");
-        findPositionOfSubStringInText(enteredText, "Java");
-        replaceSymbolsInText(enteredText, "a", "o");
+        verifyIsEndsOfSubstringText(enteredText);
+        verifyIsStartOfSubstringText(enteredText);
+        verifyIsContainsSubstringInText(enteredText);
+        findPositionOfSubstringInText(enteredText);
+        replaceSymbolsInText(enteredText);
         upperCaseForText(enteredText);
         lowerCaseForText(enteredText);
-        cutSubStringFromText(enteredText, "l", "a");
+        cutSubstringFromText(enteredText);
     }
 }
