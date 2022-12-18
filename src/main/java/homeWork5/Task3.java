f3d0a82505b95c6225092d053309b449e043cd1f
vbahno <vbahno@softserveinc.com>
2022-12-18 20:01:02 +0200

    Refactored task3 and log4j.properties

--- src/main/java/homeWork5/Task3.java
+++ src/main/java/homeWork5/Task3.java
@@ -8,22 +8,32 @@ public class Task3 {
 
     private static final Logger logger = Logger.getLogger(Task3.class);
 
-    //Ввести n слів з консолі, поки користувач не введе слово ‘Stop’. Знайти найдовше і найкоротше слово та вказати яким воно було введено(порядковий номер).
-    //Кожна дія повинна інформативно логуватись!
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String word = "";
         String maxLengthWord = word;
+        String minLengthWord = word;
+
+        int numberOfMaxPosition = 0;
+        int numberOfMinPosition = 0;
         while (!word.equalsIgnoreCase("Stop")) {
-            logger.info("Enter your word: \n");
-            word = scanner.nextLine();
+            logger.info("Enter your word:");
+            word = scanner.next();
             logger.info("User entered word: " + word);
+
             if(word.length() > maxLengthWord.length()) {
                 maxLengthWord = word;
+                numberOfMaxPosition++;
             }
-            logger.info("The longest word for now is: " + maxLengthWord);
-            logger.info("The length of the entered word is: " + word.length());
+            if(word.length() < maxLengthWord.length()) {
+                minLengthWord = word;
+                numberOfMinPosition++;
+            }
+
             logger.info("The length of the longest entered word is: " + maxLengthWord.length());
+            logger.info("Position of the longest entered word is: " + numberOfMaxPosition);
+            logger.info("The length of the shortest entered word is: " + minLengthWord.length());
+            logger.info("Position of the shortest entered word is: " + numberOfMinPosition);
         }
     }
 }
