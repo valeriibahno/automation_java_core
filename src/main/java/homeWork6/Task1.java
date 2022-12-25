167e55760182cf26d5d38277e841630e6362fb94
vbahno <vbahno@softserveinc.com>
2022-12-25 20:20:29 +0200

    Fixed Task1

--- src/main/java/homeWork6/Task1.java
+++ src/main/java/homeWork6/Task1.java
@@ -8,32 +8,35 @@ public class Task1 {
 
     private static final Logger logger = Logger.getLogger(Task1.class);
 
-    public static void verifyIsIntegerEven() {
+    public static void verifyIsIntegerEvenOrOdd() {
         Scanner scanner = new Scanner(System.in);
-        int number;
+        double number;
 
         do {
             logger.info("Enter your number:");
-            number = scanner.nextInt();
+            number  = scanner.nextDouble();
 
-                if(number%2 == 0) {
+            if(number % 2 == 0 && number % 1 == 0) {
                 logger.info("Your entered number is even");
+            }
+            else if (number % 2 == 1 && number % 1 == 0) {
+                logger.info("Your entered number is odd");
             }
             else {
                 try {
-                    throw new ArithmeticException("Your entered number is odd");
+                    throw new ArithmeticException("Your entered number is not integer");
                 } catch (ArithmeticException e) {
                     e.printStackTrace();
                 }
             }
-        } while(number < 50);
+        } while(number%2 != 0 || number%1 != 0 || number%2 != 1);
         logger.error("Program will close!");
     }
 
     public static void main(String[] args) {
 
         logger.info("Program is starting");
-        verifyIsIntegerEven();
+        verifyIsIntegerEvenOrOdd();
         logger.info("Program is finishing");
     }
 
