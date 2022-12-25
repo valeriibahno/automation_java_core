2441c99f0adb391ade4328b9ee115d6f4aa1cb79
vbahno <vbahno@softserveinc.com>
2022-12-25 20:55:14 +0200

    Fixed Task1 and Task2

--- src/main/java/homeWork6/Task1.java
+++ src/main/java/homeWork6/Task1.java
@@ -2,6 +2,7 @@ package homeWork6;
 
 import org.apache.log4j.Logger;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task1 {
@@ -12,24 +13,27 @@ public class Task1 {
         Scanner scanner = new Scanner(System.in);
         double number;
 
-        do {
-            logger.info("Enter your number:");
-            number  = scanner.nextDouble();
+        try {
+            do {
+                logger.info("Enter your number:");
+                number = scanner.nextDouble();
 
-            if(number % 2 == 0 && number % 1 == 0) {
-                logger.info("Your entered number is even");
-            }
-            else if (number % 2 == 1 && number % 1 == 0) {
-                logger.info("Your entered number is odd");
-            }
-            else {
-                try {
-                    throw new ArithmeticException("Your entered number is not integer");
-                } catch (ArithmeticException e) {
-                    e.printStackTrace();
+                if (number % 2 == 0 && number % 1 == 0) {
+                    logger.info("Your entered number is even");
+                } else if (number % 2 == 1 && number % 1 == 0) {
+                    logger.info("Your entered number is odd");
+                } else {
+                    try {
+                        throw new ArithmeticException("Your entered number is not integer");
+                    } catch (ArithmeticException e) {
+                        e.printStackTrace();
+                    }
                 }
+            } while (number % 2 != 0 || number % 1 != 0 || number % 2 != 1);
+        }
+        catch (InputMismatchException e) {
+                logger.error("You entered not double or integer value, that's why program will be closed!");
             }
-        } while(number%2 != 0 || number%1 != 0 || number%2 != 1);
         logger.error("Program will close!");
     }
 
--- src/main/java/homeWork6/Task2.java
+++ src/main/java/homeWork6/Task2.java
@@ -2,6 +2,7 @@ package homeWork6;
 
 import org.apache.log4j.Logger;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task2 {
@@ -12,28 +13,31 @@ public class Task2 {
 
         Scanner scanner = new Scanner(System.in);
 
-        int number1;
-        int number2;
+        double number1;
+        double number2;
 
-        do {
-            logger.info("Enter your number1:");
-            number1 = scanner.nextInt();
-            logger.info("Enter your number2:");
-            number2 = scanner.nextInt();
-            int result = number1 + number2;
+        try {
+            do {
+                logger.info("Enter your number1:");
+                number1 = scanner.nextDouble();
+                logger.info("Enter your number2:");
+                number2 = scanner.nextDouble();
+                double result = number1 + number2;
 
-            if(number1%2 == 0 && number2%2 == 0) {
-                logger.info("Sum of number1 and number2 = " + result);
-            }
-            else {
-                try {
-                    throw new ArithmeticException("Your entered number1 or number2 is incorrect! Try next time.");
-                } catch (ArithmeticException e) {
-                    e.printStackTrace();
+                if ((number1 % 2 == 0 && number1 % 1 == 0 || number1 % 2 == 1) || (number2 % 2 == 0 && number2 % 1 == 0 || number2 % 2 == 1)) {
+                    logger.info("Sum of number1 and number2 = " + result);
+                } else {
+                    try {
+                        throw new ArithmeticException("Your entered number1 or number2 is incorrect! Try next time.");
+                    } catch (ArithmeticException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        } while(number1%2 == 0 && number2%2 == 0);
-        logger.error("Program will close!");
+            } while (number1 % 2 != 0 || number1 % 1 != 0 || number1 % 2 != 1 || number2 % 2 != 0 || number2 % 1 != 0 || number2 % 2 != 1);
+        } catch (InputMismatchException e) {
+            logger.error("You entered not double or integer value, that's why program will be closed!");
+        }
+            logger.error("Program will close!");
     }
 
     public static void main(String[] args) {
